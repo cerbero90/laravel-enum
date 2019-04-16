a558eb14e18300f57146a88a655552008366d845
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2019-04-17 07:58:32 +1000

    Create enum definition

---
+++ src/Parsers/EnumDefinition.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Cerbero\LaravelEnum\Parsers;
+
+/**
+ * The enum definition.
+ *
+ */
+class EnumDefinition
+{
+    /**
+     * The name of the constant.
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     * The key of the enum.
+     *
+     * @var string
+     */
+    public $key;
+
+    /**
+     * The value of the enum.
+     *
+     * @var string
+     */
+    public $value;
+}
