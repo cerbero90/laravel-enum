0c9c23bc58cf54cf93355e30e7b7e524e942b550
Andrea Marco Sartori <22cerbero22@gmail.com>
2019-06-09 10:57:41 +1000

    Define keys via enum

--- src/Parser.php
+++ src/Parser.php
@@ -2,7 +2,7 @@
 
 namespace Cerbero\LaravelEnum;
 
-use Illuminate\Support\Str;
+use Closure;
 
 /**
  * The enums parser.
@@ -13,30 +13,22 @@ class Parser
     const SEPARATOR_ENUM = '|';
     const SEPARATOR_PART = '=';
 
-    private $key = 1;
-
     /**
      * Parse the enums definition
      *
      * @param string $definition
-     * @param bool $numeric
-     * @param bool $bitwise
+     * @param \Cerbero\LaravelEnum\Keys|null $keys
      * @return array
      */
-    public function parseDefinition(string $definition, bool $numeric = false, bool $bitwise = false) : array
+    public function parseDefinition(string $definition, Keys $keys = null) : array
     {
+        $resolveKeys = optional($keys)->value();
         $enums = explode(static::SEPARATOR_ENUM, $definition);
 
-        return array_map(function (string $enum) use ($numeric, $bitwise) {
+        return array_map(function (string $enum) use ($resolveKeys) {
             $parts = explode(static::SEPARATOR_PART, $enum);
 
-            if ($numeric) {
-                $parts = $this->addNumericKey($parts);
-            } elseif ($bitwise) {
-                $parts = $this->addBitwiseKey($parts);
-            }
-
-            return $this->hydrateEnumDefinition($parts);
+            return $this->hydrateEnumDefinition($parts, $resolveKeys);
         }, $enums);
     }
 
@@ -44,43 +36,23 @@ class Parser
      * Retrieve the hydrated enum definition
      *
      * @param array $parts
+     * @param \Closure|null $resolveKeys
      * @return EnumDefinition
      */
-    private function hydrateEnumDefinition(array $parts) : EnumDefinition
+    private function hydrateEnumDefinition(array $parts, Closure $resolveKeys = null) : EnumDefinition
     {
-        return tap(new EnumDefinition, function ($enumDefinition) use ($parts) {
-            $enumDefinition->name = $parts[0];
-            $enumDefinition->key = isset($parts[1]) ? $this->parseValue($parts[1]) : Str::lower($parts[0]);
-            $enumDefinition->value = isset($parts[2]) ? $this->parseValue($parts[2]) : null;
-        });
-    }
+        $enum = new EnumDefinition;
+        $enum->name = $parts[0];
 
-    /**
-     * Add a numeric enum key to the parts.
-     *
-     * @param  array  $parts
-     * @return array
-     */
-    private function addNumericKey(array $parts)
-    {
-        array_splice($parts, 1, 0, $this->key);
-        $this->key++;
+        if ($resolveKeys) {
+            $enum->key = $resolveKeys($parts[0]);
+            $enum->value = isset($parts[2]) ? $this->parseValue($parts[2]) : $this->parseValue($parts[1] ?? null);
+        } else {
+            $enum->key = isset($parts[1]) ? $this->parseValue($parts[1]) : Keys::LOWER()->value()($parts[0]);
+            $enum->value = $this->parseValue($parts[2] ?? null);
+        }
 
-        return $parts;
-    }
-
-    /**
-     * Add a bitwise enum key to the parts.
-     *
-     * @param  array  $parts
-     * @return array
-     */
-    private function addBitwiseKey(array $parts)
-    {
-        array_splice($parts, 1, 0, $this->key);
-        $this->key *= 2;
-
-        return $parts;
+        return $enum;
     }
 
     /**
