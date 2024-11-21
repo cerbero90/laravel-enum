5d47ab445449599c909fa5c0f2c9e09b2c258eb0
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2024-11-21 16:33:19 +0100

    Support enums discoverability

--- src/Enums.php
+++ src/Enums.php
@@ -7,10 +7,14 @@ namespace Cerbero\LaravelEnum;
 use Cerbero\Enum\Enums as BaseEnums;
 use Cerbero\LaravelEnum\Actions\OnCall;
 use Closure;
+use Generator;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Str;
 use UnitEnum;
 
 /**
- * The global behavior for all enums.
+ * The enums manager.
  */
 class Enums extends BaseEnums
 {
@@ -20,6 +24,13 @@ class Enums extends BaseEnums
      * @var ?Closure(UnitEnum $case, string $method): string
      */
     protected static ?Closure $translateFrom = null;
+
+    /**
+     * The glob paths to find enums in.
+     *
+     * @var string[]
+     */
+    protected static array $paths = ['app/Enums'];
 
     /**
      * Set the logic to resolve the translation key.
@@ -39,6 +50,44 @@ class Enums extends BaseEnums
         return static::$translateFrom
             ? (static::$translateFrom)($case, (string) $method)
             : sprintf('enums.%s.%s%s', $case::class, $case->name, $method ? ".{$method}" : '');
+    }
+
+    /**
+     * Set the glob paths to find all the application enums.
+     */
+    public static function paths(string ...$paths): void
+    {
+        static::$paths = $paths;
+    }
+
+    /**
+     * Yield the namespaces of all the application enums.
+     *
+     * @return Generator<int, class-string>
+     */
+    public static function namespaces(): Generator
+    {
+        $composer = json_decode((string) file_get_contents(App::basePath('composer.json')), true);
+        /** @var array<string, string> */
+        $psr4 = Arr::get((array) $composer, 'autoload.psr-4', []);
+
+        foreach (static::$paths as $relativePath) {
+            $glob = App::basePath(trim($relativePath, '/')) . '/*.php';
+
+            foreach (glob($glob) ?: [] as $path) {
+                foreach ($psr4 as $namespace => $relative) {
+                    $absolute = Str::finish(App::basePath($relative), '/');
+
+                    if (str_starts_with($path, $absolute)) {
+                        $enum = str_replace([$absolute, '/', '.php'], [$namespace, '\\', ''], $path);
+
+                        if (enum_exists($enum)) {
+                            yield $enum;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     /**
