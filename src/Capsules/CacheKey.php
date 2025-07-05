a6a41b697f4dcf6ac831786d9fe2176e1085a8de
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2025-07-05 21:36:43 +0200

    Ignore already narrowed type

--- src/Capsules/CacheKey.php
+++ src/Capsules/CacheKey.php
@@ -153,6 +153,7 @@ final class CacheKey
      */
     public function flexible(array $ttl, callable $callback, ?array $lock = null, bool $alwaysDefer = false): mixed
     {
+        /** @phpstan-ignore-next-line function.alreadyNarrowedType */
         if (method_exists(Repository::class, __FUNCTION__)) {
             /** @phpstan-ignore-next-line arguments.count */
             return Cache::flexible($this->key, $ttl, $callback, $lock, $alwaysDefer);
